83eb6ea11298139c47950a57fda3b003a2a0173c
Jordany Rosas <jordanyryan@gmail.com>
2018-01-12 12:50:57 -0500

    successfully created users and handled case when email is already in use

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,29 @@
+const User = require('../models/user');
+
 exports.signup = function(req,res,next) {
-  
+  const {email, password} = req.body;
+
+  //See if a user with given email exists
+  User.findOne({email}, function(e, existingUser) {
+    if(e) { return next(err) }
+
+  // if email exists, return error
+    if(existingUser) {
+      return res.status(422).send({error: "Email is in use"});
+    }
+
+  // if email does not exist, create and save record
+    const user = new User({
+      email,
+      password
+    })
+
+    user.save(function(err) {
+      if(err) {return next(err);}
+    });
+  // respond with successful user creation
+    res.json({success: true});
+  });
+
+
 }
